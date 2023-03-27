d926c33ba3c981b94f8a1066c6ec4a87c9afe581
isaacwrk <isaacsouzawrk@gmail.com>
2023-03-26 23:07:05 -0300

    ajustes layout

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -33,7 +33,7 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article.destroy
-    redirect_to root_path, status: :see_other
+    redirect_to root_path, status::see_other
   end
 
   private
